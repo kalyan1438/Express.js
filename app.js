00c8586e248b5145f835966769046be923073437
Gajulavarthi Kalyan Rao <kalyanrao135@gmail.com>
2026-01-01 15:23:48 +0530

    Express Day 1

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.use((req,res,next)=>{
-    console.log("Request Recived");
+    console.log("Request Recived"); // MiddleWare to handle Request
     next();
 });
 app.get("/",(req,res)=>{
     setTimeout(()=>{
-        res.send([{"Name":"Kalyan","Age":19,"Dept":"CSE"}
-            ,{"Name":"Venkatesh","Age":20,"Dept":"CSE"},
+        res.send( // Async Response has passed
+            [
+            {"Name":"Kalyan","Age":19,"Dept":"CSE"},
+            {"Name":"Venkatesh","Age":20,"Dept":"CSE"},
             {"Name":"RaviTeja","Age":20,"Dept":"CSE"},
-        {"Name":"Gowtham","Age":20,"Dept":"CSE"}])
+            {"Name":"Gowtham","Age":20,"Dept":"CSE"}
+            ])
     },3000);
 })
-app.listen(port,()=>{});
+app.listen(port,()=>{}); // Server Listening  
